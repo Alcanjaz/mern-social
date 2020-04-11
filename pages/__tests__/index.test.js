84b0ea897b70aa19dee1fba0fb1f846eb5ba6e5c
Alexis Schmoker <alexiscaposch15@gmail.com>
2020-04-10 23:11:53 -0300

    feat: added tests configuration

---
+++ pages/__tests__/index.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {mount} from 'enzyme';
+import IndexPage from '../index';
+
+describe('Index Page', () => {
+  test('should render correctly', () => {
+    const result = mount(<IndexPage/>);
+    expect(result.find('h1').text()).toBe('Hello Next.js');
+  });
+});
